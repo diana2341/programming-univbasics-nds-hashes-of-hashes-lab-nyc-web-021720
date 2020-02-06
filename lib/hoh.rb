8af17d72ad0a17d8b2707d6801cd2d903c5b2692
Diana Ponce <dianap2341@gmail.com>
2020-02-06 23:22:01 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -65,7 +65,10 @@ def naming_system
            label: "Family", 
             sub_category: { 
              label: "Genus", 
+             sub_category:{ 
+              label: "Species", 
+               sub_category: nil 
 
    
- }
+ }}}}}}}
 end
