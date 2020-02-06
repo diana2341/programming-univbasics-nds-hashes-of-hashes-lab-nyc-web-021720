cf545d52c9c2f484fa4b180f057207b531dcbab4
Diana Ponce <dianap2341@gmail.com>
2020-02-06 23:17:45 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -58,7 +58,10 @@ def naming_system
    sub_category: {
      label: "Phylum",
      sub_category: {
-       label: "Class"
+       label: "Class",
+       sub_category:{ 
+         label: "Order" 
+
    
  }
 end
