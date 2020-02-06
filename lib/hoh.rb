18ca7f34d389b3e96146848169ad559ea5a3b2e9
Diana Ponce <dianap2341@gmail.com>
2020-02-06 22:59:54 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -2,6 +2,12 @@
 # 
 # The tests will guide your construction.
 # 
+# PORTION_1 = {
+#   label: "Kingdom",
+#   sub_category: {
+#     label: "Phylum",
+#     sub_category: {
+#       label: "Class"
 #         } 
 #       } 
 #     } 
